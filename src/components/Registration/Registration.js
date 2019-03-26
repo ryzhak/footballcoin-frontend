568fea22882b9564674b2f0c56861b6b9d81a631
ryzhak <ryzhak.vladimir@gmail.com>
2019-03-27 01:51:25 +0300

    feat: add registration component

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import { Growl } from 'primereact/growl';
+import React from 'reactn';
 import { withRouter } from 'react-router-dom';
+
+import footballCoinApi from '../../lib/footballcoin-api';
 
 /**
  * User registration component
@@ -7,12 +10,42 @@ import { withRouter } from 'react-router-dom';
 class Registration extends React.Component {
 
 	/**
+	 * Component constructor
+	 * @param {Object} props Component properties 
+	 */
+	constructor(props) {
+		super(props);
+		// set initial state
+		this.state = {
+			login: '',
+			password: ''
+		};
+	}
+
+	/**
+	 * Updates state on input change
+	 * @param {Object} event Update event
+	 */
+	handleInputChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+
+	/**
 	 * Registers user and redirects him to dashboard
 	 * @param {Object} event Event object
 	 */
-	handleSubmit = (event) => {
+	handleSubmit = async (event) => {
 		event.preventDefault();
-		this.props.history.push('/dashboard/home');
+		try {
+			const resp = await footballCoinApi.register(this.state.login, this.state.password);
+			this.setGlobal({user: resp.data});
+			this.props.history.push('/dashboard/home');
+		} catch (err) {
+			console.log(err);
+			this.growl.show({severity: 'error', summary: 'Error', detail: 'User exists'});
+		}
 	};
 
 	/**
@@ -34,19 +67,32 @@ class Registration extends React.Component {
 							<h2 className="mb-4">Registration</h2>
 							<form id="loginForm" className="mt-4" onSubmit={this.handleSubmit}>
 								<div className="form-group mb-4">
-									<input type="text" name="username" placeholder="Username" className="form-control border-0 shadow form-control-lg" />
+									<input type="text" name="login" value={this.state.login} onChange={this.handleInputChange} placeholder="Username" className="form-control border-0 shadow form-control-lg" />
 								</div>
 								<div className="form-group mb-4">
-									<input type="password" name="passowrd" placeholder="Password" className="form-control border-0 shadow form-control-lg text-violet" />
+									<input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} placeholder="Password" className="form-control border-0 shadow form-control-lg text-violet" />
 								</div>
-								<button type="submit" className="btn btn-primary shadow px-5">Sign Up</button>
+								<button type="submit" className="btn btn-primary shadow px-5" disabled={!this.validateForm()}>Sign Up</button>
 							</form>
 						</div>
 					</div>
 				</div>
+				{/* Notification message component */}
+				<Growl ref={(el) => this.growl = el}></Growl>
 			</div>
 		);
 	}
+
+	/**
+	 * Validates registration form
+	 * @returns {boolean} Whether form is valid
+	 */
+	validateForm = () => {
+		let valid = true;
+		if(!this.state.login || this.state.login.length < 4) valid = false;
+		if(!this.state.password || this.state.password.length < 4) valid = false;
+		return valid;
+	};
 }
 
 export default withRouter(Registration);
--- src/lib/footballcoin-api.js
+++ src/lib/footballcoin-api.js
@@ -15,9 +15,20 @@ export async function login(login, password) {
 }
 
 /**
+ * Registers user and returns user auth details
+ * @param {string} username User login
+ * @param {string} password User password
+ *  @returns {Promise} Promise with user credentials 
+ */
+export async function register(username, password) {
+	return await axios.post(`${process.env.REACT_APP_API_URL}/users/register`, {username, password});
+}
+
+/**
  * Default object to export
  */
 export default {
-	login
+	login,
+	register
 };
 
