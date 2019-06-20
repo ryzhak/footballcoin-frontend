2c1a7a82edba3cf4207099c0e0e3f6e232333b6e
ryzhak <ryzhak.vladimir@gmail.com>
2019-06-20 04:01:03 +0300

    feat: add polls table

--- src/components/Dashboard/Poll/Poll.js
+++ src/components/Dashboard/Poll/Poll.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import moment from 'moment';
+import { Dialog } from 'primereact/dialog';
+import React from 'reactn';
+
+import pollSystemContract from '../../../lib/poll-system-contract';
 
 /**
  * Poll component
@@ -6,12 +10,127 @@ import React from 'react';
 export default class Poll extends React.Component {
 
 	/**
+	 * Component constructor
+	 * @param {Object} props Component properties 
+	 */
+	constructor(props) {
+		super(props);
+		// initial state
+		this.state = {
+			currentVoting: {
+				choices: []
+			},
+			displayVoteDialog: false,
+			votings: []
+		};
+	}
+
+	/**
+	 * On component finish render
+	 */
+	componentDidMount() {
+		setTimeout(() => {
+			this.init();	
+		}, 500);
+	}
+
+	/**
+	 * Initializes component
+	 */
+	init = async () => {
+		const votings = await pollSystemContract.getVotings();
+		this.setState({votings});
+	};
+
+	/**
 	 * Renders component template
+	 * @returns {Object} JSX template
 	 */
 	render() {
 		return (
-			<h1 className="py-5">Polls</h1>
+			<React.Fragment>
+				{/* polls section */}
+				<section className="my-5">
+					<div className="row">
+						<div className="col-lg-6 mb-4">
+							<div className="card">
+								<div className="card-header">
+									<h6 className="text-uppercase mb-0">Polls</h6>
+								</div>
+								<div className="card-body">
+									{/* polls table */}
+									<table className="table table-striped table-hover card-text">
+										<thead>
+											<tr>
+												<th>#</th>
+												<th>Question</th>
+												<th>Available until</th>
+												<th>Quorum rate</th>
+												<th>Consensus rate</th>
+												<th></th>
+											</tr>
+										</thead>
+										<tbody>
+											{this.state.votings.map((voting, index) => 
+												<tr key={ index }>
+													<th scope="row">{ index + 1 }</th>
+													<td>{ voting.question }</td>
+													<td>{ moment(voting.availableUntil * 1000).format('lll') }</td>
+													<td>{ voting.quorumRate / 100 }%</td>
+													<td>{ voting.consensusRate / 100 }%</td>
+													<td className="text-right">
+														<button onClick={(e) => this.showVoteDialog(voting)} type="button" className="btn btn-primary shadow px-5 mr-1">View</button>
+													</td>
+												</tr>
+											)}
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				{/* vote dialog */}
+				<Dialog header="Votes" visible={this.state.displayVoteDialog} style={{width: '50vw'}} onHide={(e) => this.setState({displayVoteDialog: false})}>
+					<div className="card">
+						<div className="card-body">
+							{/* votes table */}
+							<table className="table table-striped table-hover card-text">
+								<thead>
+									<tr>
+										<th>#</th>
+										<th>Choice</th>
+										<th>Votes count</th>
+										<th></th>
+									</tr>
+								</thead>
+								<tbody>
+									{this.state.currentVoting.choices.map((choice, index) => 
+										<tr key={ index }>
+											<th scope="row">{ index + 1 }</th>
+											<td>{ choice.text }</td>
+											<td>{ choice.votesCount }</td>
+											<td>Action</td>
+										</tr>
+									)}
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</Dialog>
+			</React.Fragment>
 		);
 	}
+
+	/**
+	 * Displays vote dialog
+	 * @param {Object} voting voting that was clicked
+	 */
+	showVoteDialog = (voting) => {
+		this.setState({
+			currentVoting: {...voting},
+			displayVoteDialog: true
+		});
+	};
 }
 
--- src/lib/poll-system-contract.js
+++ src/lib/poll-system-contract.js
@@ -37,6 +37,24 @@ export async function removeUser(userAddress) {
 //***************
 
 /**
+ * Returns array of choices from voting
+ * @param {number} index voting index
+ * @returns {Promise} promise with array of choices 
+ */
+export async function getChoicesFromVoting(index) {
+	return await contract.methods.getChoicesFromVoting(index).call();
+}
+
+/**
+ * Returns array of votes count per choice
+ * @param {number} index voting index
+ * @returns {Promise} promise with array of votes count per choice 
+ */
+export async function getVotesCount(index) {
+	return await contract.methods.getVotesCount(index).call();
+}
+
+/**
  * Returns user address by index
  * @param {number} index user index
  * @returns {Promise} promise with user address 
@@ -51,6 +69,23 @@ export async function users(index) {
  */
 export async function usersCount() {
 	return (await contract.methods.usersCount().call()).toNumber();
+}
+
+/**
+ * Returns voting data by index
+ * @param {number} index voting index
+ * @returns {Promise} promise with voting data 
+ */
+export async function votings(index) {
+	return await contract.methods.votings(index).call();
+}
+
+/**
+ * Returns number of votings
+ * @returns {Promise} promise with number of votings
+ */
+export async function votingsCount() {
+	return (await contract.methods.votingsCount().call()).toNumber();
 }
 
 //***************
@@ -72,13 +107,52 @@ export async function getUsers() {
 }
 
 /**
+ * Returns all votings
+ * @returns {Promise} promise with all votings
+ */
+export async function getVotings() {
+	let votingsArray = [];
+	const count = await votingsCount();
+	// for all votings
+	for(let i = 0; i < count; i++) {
+		const votingInfo = await votings(0);
+		// get choices
+		let choices = [];
+		const texts = await getChoicesFromVoting(i);
+		const votesCount = await getVotesCount(i);
+		for(let j = 0; j < votingInfo.choicesCount.toNumber(); j++) {
+			choices.push({
+				text: texts[j],
+				votesCount: votesCount[j].toNumber()
+			});
+		}
+		// set voting params and choices
+		votingsArray.push({
+			availableUntil: votingInfo.availableUntil.toNumber(),
+			choicesCount: votingInfo.choicesCount.toNumber(),
+			consensusRate: votingInfo.consensusRate.toNumber(),
+			createdAt: votingInfo.createdAt.toNumber(),
+			question: votingInfo.question,
+			quorumRate: votingInfo.quorumRate.toNumber(),
+			choices: choices
+		});
+	}
+	return votingsArray;
+}
+
+/**
  * Default object to export
  */
 export default {
 	addUser,
+	getChoicesFromVoting,
 	getUsers,
+	getVotesCount,
+	getVotings,
 	init,
 	removeUser,
 	users,
-	usersCount
+	usersCount,
+	votings,
+	votingsCount
 };
